de394eae05ab0d20253b9b324a3281483b273e2c
EdenChien <tsen090981@gmail.com>
2023-09-03 16:47:08 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/w5-TicketWeb-v2/',
+  base: '/w6-TicketWeb/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
